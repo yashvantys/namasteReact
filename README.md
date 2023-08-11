f7f53a5b4a23c45b0f997d9444723a9a1534547b
Yashvant Yadav <yashvanty@gmail.com>
2023-08-11 14:15:47 +0530

    Lession 2 changes and assignment

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# namasteReact
-# This is my React js 
+# React Learnng
+
+
+
+# Parcel
+-dev build
+-local server
+-HMR - Hot Module Replacement
+-File watching algorithm - written in c++
+-Caching - faster build
+-Image optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hasing
+-Code spliting
+-Differential Bundling - support older browser
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundle
+
+
--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-/**
- * 
- * nested tags
- * <div id="parent">
- *      <div id="child1">
- *          <h1>I'am in h1 tag</h1>
- *          <h2>I'am in h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>I'am in h1 tag</h1>
- *          <h2>I'am in h2 tag</h2>
- *      </div>
- * </div>
- * 
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
 const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'am in h1 tag"), React.createElement('h2', {}, 'I am in h2 tag')]), React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "I'am in h1 tag"), React.createElement('h2', {}, 'I am in h2 tag')])]);
 const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
 console.log(parent)
